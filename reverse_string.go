e322b7ebeee52a20bda21d422a9d3d2eb6fa1c0c
Nurlan <ikhsan.nurlan@gmail.com>
2022-05-15 15:18:17 +0600

    fix: international symbols

--- reverse_string.go
+++ reverse_string.go
@@ -1,8 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	for i := len(input) - 1; i >= 0; i-- {
-		output += string(input[i])
+	rr := []rune(input)
+	for i := len(rr) - 1; i >= 0; i-- {
+		output += string(rr[i])
 	}
 	return output
 }
--- reverse_string_test.go
+++ reverse_string_test.go
@@ -1,14 +1,22 @@
 package reverse_string
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestReverseString(t *testing.T) {
-	val := "Hello world!"
+	val := "привет"
+	expected := []rune("тевирп")
 
-	expected := "!dlrow olleH"
-	got := ReverseString(val)
+	got := []rune(ReverseString(val))
 
-	if expected != got {
-		t.Errorf("reverseString(), got: %v, expected: %v", got, expected)
+	for i := 0; i < len(got); i++ {
+		for j := i; j < len(expected)-1; j++ {
+			if got[i] == expected[j] {
+				break
+			} else {
+				t.Errorf("reverseString(), got: %v, expected: %v", got, expected)
+			}
+		}
 	}
 }
